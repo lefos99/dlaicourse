905df94ef06485f2a694f6e8193b94b207381713
lefos99 <lefosm@gmail.com>
2020-04-25 16:35:27 +0200

    TensorFlow Deployment/Course 1 - TensorFlow-JS is done! :D

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
@@ -25,20 +25,20 @@ async function train() {
   // using ReLu activation functions where applicable.
   model = tf.sequential({
     layers: [
-        
-      // YOUR CODE HERE
-
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({ units: 100, activation: 'relu'}),
+      tf.layers.dense({ units: 5, activation: 'softmax'})
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001); // YOUR CODE HERE
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});// YOUR CODE HERE);
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -70,6 +70,10 @@ function handleButton(elem){
 		case "3":
 			spockSamples++;
 			document.getElementById("spocksamples").innerText = "Spock samples:" + spockSamples;
+			break;
+        case "4":
+			lizardSamples++;
+			document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
 			break;
             
         // Add a case for lizard samples.
@@ -108,7 +112,10 @@ async function predict() {
 		case 3:
 			predictionText = "I see Spock";
 			break;
-            
+        case 4:
+			predictionText = "I see Lizard";
+			break;
+        
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
             
